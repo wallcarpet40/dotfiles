8feca4485ab51c6487e608526c9a3178a8b756e6
wallcarpet40 <terssi@mbnet.fi>
2021-03-03 22:39:11 +0200

    added scripts for Zwift and Opentrack

---
+++ .local/share/applications/z.sh
@@ -0,0 +1,7 @@
+WINEPREFIX=/home/htpc/Games/zwift /home/htpc/.local/share/lutris/runners/wine/lutris-6.0-x86_64/bin/wine "/home/htpc/Games/zwift/drive_c/Program Files (x86)/Zwift/ZwiftLauncher.exe" &
+sleep 3
+WINEPREFIX=/home/htpc/Games/zwift /home/htpc/.local/share/lutris/runners/wine/lutris-6.0-x86_64/bin/wine /home/htpc/Games/zwift/RunFromProcess-x64.exe ZwiftLauncher.exe ZwiftApp.exe &
+
+
+
+
